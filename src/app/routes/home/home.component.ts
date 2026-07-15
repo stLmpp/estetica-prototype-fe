02a2522d4d5e75cd02fea9d626e621c76d11ad8a
stLmpp <gui.stlmpp@gmail.com>
2026-07-14 23:24:46 -0300

    feat: temp

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,22 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
 import { AuthService } from 'ngx-better-auth';
 import { JsonPipe } from '@angular/common';
 import { ButtonComponent } from '../../components/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [JsonPipe, ButtonComponent],
+  imports: [JsonPipe, ButtonComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   readonly authService = inject(AuthService);
 
+  username = model('');
+  password = model('');
+
   protected login() {
     this.authService
       .signInEmail({
-        email: '',
-        password: '',
+        email: this.username(),
+        password: this.password(),
         rememberMe: true,
       })
       .subscribe();
