4159fb9fdb9c1c211adebc001d947d885153e531
stLmpp <gui.stlmpp@gmail.com>
2026-08-08 01:01:50 -0300

    feat: started working on side bar menu

---
+++ src/app/core/sidebar/sidebar-menu-item/sidebar-menu-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IconComponent } from '../../../components/icon/icon.component';
+import { LucideIconInput } from '@lucide/angular';
+
+@Component({
+  selector: 'app-sidebar-menu-item',
+  imports: [RouterLink, RouterLinkActive, IconComponent],
+  templateUrl: './sidebar-menu-item.component.html',
+  styleUrl: './sidebar-menu-item.component.css',
+})
+export class SidebarMenuItemComponent {
+  readonly title = input.required<string>();
+  readonly link = input.required<string>();
+  readonly linkActiveOptions = input<RouterLinkActive['routerLinkActiveOptions']>();
+  readonly icon = input<LucideIconInput>();
+}
